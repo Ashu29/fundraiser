095a10e0520dc241752ea9d013eb6254ebbb34cc
Ashu29 <ashu29992@gmail.com>
2017-08-29 21:09:07 +0530

    Exposes Profile ID in django admin

--- company/admin.py
+++ company/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from company import models
 # Register your models here.
-admin.site.register(models.Company)
 admin.site.register(models.Market)
 admin.site.register(models.FundingDetail)
+
+
+@admin.register(models.Company)
+class CompanyAdmin(admin.ModelAdmin):
+    readonly_fields=('profile_id',)
